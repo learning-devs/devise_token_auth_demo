3aa5252558827e79f1a63578bb5a0b7d7e2789a2
EmmanuelSW <edhgt89@hotmail.com>
2017-07-17 16:49:06 -0500

    upload image example

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :recoverable, :rememberable, :trackable, :validatable
+          # :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
