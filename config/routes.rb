e755cc6a7db52681d9adcded7f35cb325168fb1e
MIGUEL ANGEL ARENAS CORREA <marenaa@contratista.epm.co>
2017-07-18 12:56:16 -0500

    Ajuste de rutas, auth a /api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :products
-  mount_devise_token_auth_for 'User', at: 'auth'
+	
 
-  namespace :api do
-    namespace :v1 do
-      get 'users', to: 'users#index'
-      resources :products
-    end
-  end
+	namespace :api do
+		namespace :v1 do
+			get 'users', to: 'users#index'
+			resources :products
+			mount_devise_token_auth_for 'User', at: 'auth'
+		end
+	end
 
 end
