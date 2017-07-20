c8cdc928dda3c7fed7c2e933ecd792e85bede61c
EmmanuelSW <edhgt89@hotmail.com>
2017-07-20 18:03:42 -0500

    Configure host and email_sender for devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => '289a903d4d2fa4',
---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+Devise.setup do |config|
+  config.mailer_sender = 'test_api_devise@learningrails.com'
+end
